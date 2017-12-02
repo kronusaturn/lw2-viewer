812a4df59f0b75c91cf0ae604bc5a7b87b9ffc1f
saturn <saturn@glxl.net>
2017-12-02 17:07:13 -0600

    Use cl-unicode to catch all unicode forms of whitespace.

--- lw2.lisp
+++ lw2.lisp
@@ -332,7 +332,7 @@
 					   (when new-link
 					     (setf (plump:attribute node "href") new-link))))))
 				   (when (tag-is node "p" "blockquote" "div")
-				     (when (every (lambda (c) (find c '(#\Space #\Newline #\Tab #\Return #\Linefeed #\Page))) (plump:text node)) 
+				     (when (every (lambda (c) (cl-unicode:has-binary-property c "White_Space")) (plump:text node)) 
 				       (plump:remove-child node)))
 				   (when (and with-toc (ppcre:scan "^h[1-6]$" (plump:tag-name node)))
 				     (incf section-count) 
