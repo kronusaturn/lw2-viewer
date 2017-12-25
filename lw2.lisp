239796fd89ccedc10a46eff0a3a120d1dfee311e
saturn <saturn@glxl.net>
2017-12-24 20:50:19 -0600

    Use fonts.greaterwrong.com font server.

--- lw2.lisp
+++ lw2.lisp
@@ -451,7 +451,7 @@
 
 (defparameter *html-head*
 "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
-<link rel=\"stylesheet\" href=\"https://fonts.obormot.net?fonts=Charter,Geometric415\">")
+<link rel=\"stylesheet\" href=\"//fonts.greaterwrong.com/?fonts=Charter,Geometric415\">")
 
 (defun generate-versioned-link (file)
   (format nil "~A?v=~A" file (sb-posix:stat-mtime (sb-posix:stat (format nil "www~A" file))))) 
