e75e88debe4f9f7310658b4118148928d16d7a33
Said Achmiz <said@saidachmiz.net>
2018-01-06 17:44:15 -0500

    Possible improvement to deeply nested comment styling

--- www/style.css.php
+++ www/style.css.php
@@ -718,6 +718,16 @@ h1.listing a[href^="/"]::after {
 	max-height: 1000000px;
 }
 
+.comment-item input[id^="expand"]:checked ~ .comment-thread {
+	font-size: 0.95em;
+}
+.comment-item input[id^="expand"]:checked ~ .comment-thread .comment-thread {
+	font-size: 0.975em;
+}
+.comment-item input[id^="expand"]:checked ~ .comment-thread .comment-body {
+	font-size: 1.2em;
+}
+
 <?php
 	function nested_stuff($segment, $tip, $last_tip, $nesting_levels) {
 		for ($i = $nesting_levels; $i > 0; $i--) {
@@ -729,7 +739,7 @@ h1.listing a[href^="/"]::after {
 	}
 ?>
 
-<?php nested_stuff(".comment-item .comment-item ", ".comment-item,\n", ".comment-item", 7); ?> {
+<?php nested_stuff(".comment-item .comment-item ", ".comment-item,\n", ".comment-item", 10); ?> {
 	background-color: #eee;
 }
 
@@ -737,7 +747,7 @@ h1.listing a[href^="/"]::after {
 	background-color: #fff;
 }
 
-<?php nested_stuff(".comment-item ", ".comment-item:target,\n", ".comment-item:target", 13); ?> {
+<?php nested_stuff(".comment-item ", ".comment-item:target,\n", ".comment-item:target", 19); ?> {
 	background-color: #ffd;
 }
 
