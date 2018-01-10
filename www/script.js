faf03475d71b82d4299d5a3bc2f87c469a27af8c
Said Achmiz <said@saidachmiz.net>
2018-01-10 17:26:38 -0500

    Added JS to hide inapplicable quicknav buttons

--- www/script.js
+++ www/script.js
@@ -1,11 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-	if(document.getElementById("content").clientHeight <= window.innerHeight + 30) {
-		let e = document.getElementById("bottom-bar");
-		e.parentNode.removeChild(e);
+	var content = document.querySelector("#content");
+	if (content.clientHeight <= window.innerHeight + 30) {
+		content.removeChild(document.querySelector("#bottom-bar");
+		document.querySelector(".post .post-meta").removeChild(document.querySelector(".post .post-meta a[href='#bottom-bar']"));
+	} 	
+	if (content.clientHeight <= window.innerHeight + 30 || 
+		document.querySelector("#comments").childNodes.length == 0) {
+		window.document.styleSheets[1].insertRule('.post .post-meta .comment-count::after { display: none; }', window.document.styleSheets[1].cssRules.length);
 	}
+
 	let urlParts = document.URL.split('#');
 	if (urlParts.length > 1) {
-		document.querySelector('#'+urlParts[1]).closest("label[for^='expand'] + .comment-thread").parentElement.querySelector("input[id^='expand']").checked = true;
+		try { document.querySelector('#'+urlParts[1]).closest("label[for^='expand'] + .comment-thread").parentElement.querySelector("input[id^='expand']").checked = true; }
+		catch (e) { }
 		location.hash = '';
 		location.hash = urlParts[1];
 	}
