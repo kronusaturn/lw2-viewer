76bf6a49da800fe9564feb50693277b1ecf4d00f
saturn <saturn@glxl.net>
2018-01-10 20:06:23 -0600

    Fix FOUT problem in firefox.

--- www/script.js
+++ www/script.js
@@ -1,19 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
-	var content = document.querySelector("#content");
-	if (content.clientHeight <= window.innerHeight + 30) {
-		content.removeChild(document.querySelector("#bottom-bar"));
-		document.querySelector(".post .post-meta").removeChild(document.querySelector(".post .post-meta a[href='#bottom-bar']"));
-	} 	
-	if (content.clientHeight <= window.innerHeight + 30 || 
-		document.querySelector("#comments").childNodes.length == 0) {
-		document.styleSheets[1].insertRule('.post .post-meta .comment-count::after { display: none; }', document.styleSheets[1].cssRules.length);
-	}
+	window.requestAnimationFrame(function() {
+		var content = document.querySelector("#content");
+		if (content.clientHeight <= window.innerHeight + 30) {
+			content.removeChild(document.querySelector("#bottom-bar"));
+			document.querySelector(".post .post-meta").removeChild(document.querySelector(".post .post-meta a[href='#bottom-bar']"));
+		} 	
+		if (content.clientHeight <= window.innerHeight + 30 || 
+			document.querySelector("#comments").childNodes.length == 0) {
+			document.styleSheets[1].insertRule('.post .post-meta .comment-count::after { display: none; }', document.styleSheets[1].cssRules.length);
+		}
 
-	let urlParts = document.URL.split('#');
-	if (urlParts.length > 1) {
-		try { document.querySelector('#'+urlParts[1]).closest("label[for^='expand'] + .comment-thread").parentElement.querySelector("input[id^='expand']").checked = true; }
-		catch (e) { }
-		location.hash = '';
-		location.hash = urlParts[1];
-	}
+		let urlParts = document.URL.split('#');
+		if (urlParts.length > 1) {
+			try { document.querySelector('#'+urlParts[1]).closest("label[for^='expand'] + .comment-thread").parentElement.querySelector("input[id^='expand']").checked = true; }
+			catch (e) { }
+			location.hash = '';
+			location.hash = urlParts[1];
+		}
+	})
 });
