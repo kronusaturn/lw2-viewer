8e79eb40df9c7365b8b6f2d23565bccff51a5bf2
Said Achmiz <said@saidachmiz.net>
2018-01-12 20:55:16 -0500

    Fix inconsistency in background color of focused comments

--- www/style.css.php
+++ www/style.css.php
@@ -850,7 +850,7 @@ h1.listing a[href^="/"]::after {
 		4px  4px 0 12px #ffd inset;
 }
 
-<?php nested_stuff(".comment-item ", ".comment-item:target,\n", ".comment-item:target", (2 * $comment_nesting_depth) - 1); ?> {
+<?php nested_stuff(".comment-item ", ".comment-item:target:not(:focus-within),\n", ".comment-item:target:not(:focus-within)", (2 * $comment_nesting_depth) - 1); ?> {
 	background-color: #ffd;
 }
 <?php nested_stuff(".comment-item ", ".comment-item:target .comment-item a.comment-parent-link::after,\n", ".comment-item:target .comment-item a.comment-parent-link::after", (2 * $comment_nesting_depth) - 1); ?> {
