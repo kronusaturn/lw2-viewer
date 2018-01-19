77b1246b17e3b82ea6031d4c73ae727bd3862132
saturn <saturn@glxl.net>
2018-01-19 01:27:20 -0600

    Add try/catch around use of Intl in script.js

--- www/script.js
+++ www/script.js
@@ -159,11 +159,14 @@ document.addEventListener("DOMContentLoaded", function() {
 			document.styleSheets[1].insertRule('.post .post-meta .comment-count::after { display: none; }', document.styleSheets[1].cssRules.length);
 		}
 
-		let dtf = new Intl.DateTimeFormat([], {month: 'short', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric'});
-		document.querySelectorAll(".date").forEach(function (e) {
-			let d = e.getAttribute("data-js-date");
-			if(d) { e.innerHTML = dtf.format(new Date(+ d)); }
-		});
+		try {
+			let dtf = new Intl.DateTimeFormat([], {month: 'short', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric'});
+			document.querySelectorAll(".date").forEach(function (e) {
+				let d = e.getAttribute("data-js-date");
+				if(d) { e.innerHTML = dtf.format(new Date(+ d)); }
+			});
+		}
+		catch(e) { }
 
 		let needHashRealignment = false;
 
