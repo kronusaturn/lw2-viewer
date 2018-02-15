01e7ff6a4fa9aba03b99fc3a12418e2107abf1de
Said Achmiz <said@saidachmiz.net>
2018-02-15 12:56:57 -0500

    Improved rendering of theme tweaker hint

--- www/script.js
+++ www/script.js
@@ -476,7 +476,7 @@ function injectThemeTweaker() {
 		<button type='button' class='cancel-button'>Cancel</button>
 	</div>
 	<div class="clippy-container">
-        <span class="hint">Hi, I’m Bobby the Basilisk! Click on the minimize button (<img src='minimize_button_icon.gif' />) to minimize the theme tweaker window, so that you can see what the page looks like with the current tweaked values. (But remember, <span>the changes won’t be saved until you click “OK”!</span>)
+        <span class="hint">Hi, I'm Bobby the Basilisk! Click on the minimize button (<img src='minimize_button_icon.gif' />) to minimize the theme tweaker window, so that you can see what the page looks like with the current tweaked values. (But remember, <span>the changes won't be saved until you click "OK"!</span>)
         <img class='clippy' src='basilisk.png' />
     </div>
 	` + "</div></div>");
