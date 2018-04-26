78fddbe515cf0c61a936a6c219b43210aacc91b5
saturn <saturn@glxl.net>
2018-04-25 21:48:53 -0500

    Add thousands separators to word count.

--- lw2.lisp
+++ lw2.lisp
@@ -70,7 +70,7 @@
                (draft boolean))
     post
     (multiple-value-bind (pretty-time js-time) (pretty-time posted-at)
-      (format out-stream "<h1 class=\"listing~:[~; link-post-listing~]\">~@[<a href=\"~A\">&#xf0c1;</a>~]<a href=\"~A\">~A</a></h1><div class=\"post-meta\"><a class=\"author\" href=\"/users/~A\">~A</a> <div class=\"date\" data-js-date=\"~A\">~A</div><div class=\"karma\"><span class=\"karma-value\">~A</span></div><a class=\"comment-count\" href=\"~A#comments\">~A comment~:P</a>~:[~*~;~:*<span class=\"read-time\" title=\"~A word~:P\">~A min read</span>~]~@[<a class=\"lw2-link\" href=\"~A\">LW link</a>~]~1{<span class=\"post-section ~A\" title=\"~A\">~:*~A</span>~}~A</div>"
+      (format out-stream "<h1 class=\"listing~:[~; link-post-listing~]\">~@[<a href=\"~A\">&#xf0c1;</a>~]<a href=\"~A\">~A</a></h1><div class=\"post-meta\"><a class=\"author\" href=\"/users/~A\">~A</a> <div class=\"date\" data-js-date=\"~A\">~A</div><div class=\"karma\"><span class=\"karma-value\">~A</span></div><a class=\"comment-count\" href=\"~A#comments\">~A comment~:P</a>~:[~*~;~:*<span class=\"read-time\" title=\"~:D word~:P\">~:D min read</span>~]~@[<a class=\"lw2-link\" href=\"~A\">LW link</a>~]~1{<span class=\"post-section ~A\" title=\"~A\">~:*~A</span>~}~A</div>"
               url
               (if url (encode-entities (string-trim " " url)))
               (generate-post-auth-link post nil nil need-auth)
