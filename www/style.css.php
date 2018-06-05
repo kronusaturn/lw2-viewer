a57961d6277a3fd64ea0ffb4c107a06ace068033
Said Achmiz <said@saidachmiz.net>
2018-06-05 14:18:56 -0400

    Added Less theme button

--- www/style.css.php
+++ www/style.css.php
@@ -469,6 +469,9 @@ a#inbox-indicator.new-messages:hover::before {
 .theme-selector button:nth-of-type(8) {
 	background-image: url('data:image/gif;base64,<?php echo base64_encode(file_get_contents("assets/theme_H.gif")) ?>');
 }
+.theme-selector button:nth-of-type(9) {
+	background-image: url('data:image/gif;base64,<?php echo base64_encode(file_get_contents("assets/theme_I.gif")) ?>');
+}
 .theme-selector button:disabled {
 	cursor: auto;
 }
