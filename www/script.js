8deac4d0edee9e639b6a17193d712d2ec5430c9a
Said Achmiz <achmizs@gmail.com>
2018-10-30 13:14:59 -0400

    Slight correction to new-comment-nav button tooltips

--- www/script.js
+++ www/script.js
@@ -1355,9 +1355,9 @@ function injectQuickNavUI() {
 
 function injectNewCommentNavUI(newCommentsCount) {
 	let newCommentUIContainer = addUIElement("<div id='new-comment-nav-ui'>" + 
-	`<button type='button' class='new-comment-sequential-nav-button new-comment-previous' title='Previous new comment [,]' tabindex='-1'>&#xf0d8;</button>
+	`<button type='button' class='new-comment-sequential-nav-button new-comment-previous' title='Previous new comment (,)' tabindex='-1'>&#xf0d8;</button>
 	<span class='new-comments-count'></span>
-	<button type='button' class='new-comment-sequential-nav-button new-comment-next' title='Next new comment [.]' tabindex='-1'>&#xf0d7;</button>`
+	<button type='button' class='new-comment-sequential-nav-button new-comment-next' title='Next new comment (.)' tabindex='-1'>&#xf0d7;</button>`
 	+ "</div>");
 
 	newCommentUIContainer.querySelector(".new-comment-previous").addActivateEvent(commentQuicknavButtonClicked);
