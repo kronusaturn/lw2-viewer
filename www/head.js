kronusaturn/lw2-viewer

commit 681dd89c161c2ef1bc7e5f2657434338b6240e5b

Corrected bug with comment parent popups vs. filters, part II

www/head.js
@@ -173,6 +173,8 @@ function applyFilters(filters) {
 		let filtersExclusionTree = exclusionTreeFromExclusionPaths(GW.themeTweaker.filtersExclusionPaths) || GW.themeTweaker.defaultFiltersExclusionTree;
 		console.log(selectorFromExclusionTree(filtersExclusionTree));
 		fullStyleString = `body::before { content: ""; } ${selectorFromExclusionTree(filtersExclusionTree)} { filter: ${filterStringFromFilters(filters)}; }`;
+	} else {
+		fullStyleString = `#content::before { z-index: -1; } `;
 	}
 	
 	// Update the style tag (if it’s already been loaded).

www/style-brutalist.linux.css
@@ -53,7 +53,6 @@ body > * {
 	width: 100%;
 	height: 100%;
 	pointer-events: none;
-	z-index: -1;
 }
 
 /*=---------=*/

www/style-brutalist.mac.css
@@ -53,7 +53,6 @@ body > * {
 	width: 100%;
 	height: 100%;
 	pointer-events: none;
-	z-index: -1;
 }
 
 /*=---------=*/

www/style-brutalist.windows.css
@@ -53,7 +53,6 @@ body > * {
 	width: 100%;
 	height: 100%;
 	pointer-events: none;
-	z-index: -1;
 }
 
 /*=---------=*/

www/style-classic.linux.css
@@ -53,7 +53,6 @@ body > * {
 	width: 100%;
 	height: 100%;
 	pointer-events: none;
-	z-index: -1;
 }
 
 /*=---------=*/

www/style-classic.mac.css
@@ -53,7 +53,6 @@ body > * {
 	width: 100%;
 	height: 100%;
 	pointer-events: none;
-	z-index: -1;
 }
 
 /*=---------=*/

www/style-classic.windows.css
@@ -53,7 +53,6 @@ body > * {
 	width: 100%;
 	height: 100%;
 	pointer-events: none;
-	z-index: -1;
 }
 
 /*=---------=*/

www/style-dark.linux.css
@@ -53,7 +53,6 @@ body > * {
 	width: 100%;
 	height: 100%;
 	pointer-events: none;
-	z-index: -1;
 }
 
 /*=---------=*/

commit a4b3c9e65e9178a04595fc01b664f48c4840537b

Fixed bug caused by 681dd89c161c2ef1bc7e5f2657434338b6240e5b

www/head.js
@@ -175,9 +175,7 @@ function applyFilters(filters) {
 	
 	if (!filters.isEmpty()) {
 		let filtersExclusionTree = exclusionTreeFromExclusionPaths(GW.themeTweaker.filtersExclusionPaths) || GW.themeTweaker.defaultFiltersExclusionTree;
-		fullStyleString = `body::before { content: ""; } ${selectorFromExclusionTree(filtersExclusionTree)} { filter: ${filterStringFromFilters(filters)}; }`;
-	} else {
-		fullStyleString = `#content::before { z-index: -1; } `;
+		fullStyleString = `body::before { content: ""; } body > #content::before { z-index: 0; } ${selectorFromExclusionTree(filtersExclusionTree)} { filter: ${filterStringFromFilters(filters)}; }`;
 	}
 	
 	// Update the style tag (if it’s already been loaded).

www/style-brutalist.linux.css
@@ -52,6 +52,7 @@ body > * {
 	left: 0;
 	width: 100%;
 	height: 100%;
+	z-index: -1;
 	pointer-events: none;
 }
 

www/style-brutalist.mac.css
@@ -52,6 +52,7 @@ body > * {
 	left: 0;
 	width: 100%;
 	height: 100%;
+	z-index: -1;
 	pointer-events: none;
 }
 

www/style-brutalist.windows.css
@@ -52,6 +52,7 @@ body > * {
 	left: 0;
 	width: 100%;
 	height: 100%;
+	z-index: -1;
 	pointer-events: none;
 }
 

www/style-classic.linux.css
@@ -52,6 +52,7 @@ body > * {
 	left: 0;
 	width: 100%;
 	height: 100%;
+	z-index: -1;
 	pointer-events: none;
 }
 

www/style-classic.mac.css
@@ -52,6 +52,7 @@ body > * {
 	left: 0;
 	width: 100%;
 	height: 100%;
+	z-index: -1;
 	pointer-events: none;
 }
 

www/style-classic.windows.css
@@ -52,6 +52,7 @@ body > * {
 	left: 0;
 	width: 100%;
 	height: 100%;
+	z-index: -1;
 	pointer-events: none;
 }
 

www/style-dark.linux.css
@@ -52,6 +52,7 @@ body > * {
 	left: 0;
 	width: 100%;
 	height: 100%;
+	z-index: -1;
 	pointer-events: none;
 }
 

www/style-dark.mac.css
@@ -52,6 +52,7 @@ body > * {
 	left: 0;
 	width: 100%;
 	height: 100%;
+	z-index: -1;
 	pointer-events: none;
 }
 

www/style-dark.windows.css
@@ -52,6 +52,7 @@ body > * {
 	left: 0;
 	width: 100%;
 	height: 100%;
+	z-index: -1;
 	pointer-events: none;
 }
 

www/style-grey.linux.css
@@ -52,6 +52,7 @@ body > * {
 	left: 0;
 	width: 100%;
 	height: 100%;
+	z-index: -1;
 	pointer-events: none;
 }
 

www/style-grey.mac.css
@@ -52,6 +52,7 @@ body > * {
 	left: 0;
 	width: 100%;
 	height: 100%;
+	z-index: -1;
 	pointer-events: none;
 }
 

www/style-grey.windows.css
@@ -52,6 +52,7 @@ body > * {
 	left: 0;
 	width: 100%;
 	height: 100%;
+	z-index: -1;
 	pointer-events: none;
 }
 

www/style-less.linux.css
@@ -52,6 +52,7 @@ body > * {
 	left: 0;
 	width: 100%;
 	height: 100%;
+	z-index: -1;
 	pointer-events: none;
 }
 

www/style-less.mac.css
@@ -52,6 +52,7 @@ body > * {
 	left: 0;
 	width: 100%;
 	height: 100%;
+	z-index: -1;
 	pointer-events: none;
 }
 

www/style-less.windows.css
@@ -52,6 +52,7 @@ body > * {
 	left: 0;
 	width: 100%;
 	height: 100%;
+	z-index: -1;
 	pointer-events: none;
 }
 

www/style-rts.linux.css
@@ -52,6 +52,7 @@ body > * {
 	left: 0;
 	width: 100%;
 	height: 100%;
+	z-index: -1;
 	pointer-events: none;
 }
 

www/style-rts.mac.css
@@ -52,6 +52,7 @@ body > * {
 	left: 0;
 	width: 100%;
 	height: 100%;
+	z-index: -1;
 	pointer-events: none;
 }
 

www/style-rts.windows.css
@@ -52,6 +52,7 @@ body > * {
 	left: 0;
 	width: 100%;
 	height: 100%;
+	z-index: -1;
 	pointer-events: none;
 }
 

www/style-ultramodern.linux.css
@@ -52,6 +52,7 @@ body > * {
 	left: 0;
 	width: 100%;
 	height: 100%;
+	z-index: -1;
 	pointer-events: none;
 }
 

www/style-ultramodern.mac.css
@@ -52,6 +52,7 @@ body > * {
 	left: 0;
 	width: 100%;
 	height: 100%;
+	z-index: -1;
 	pointer-events: none;
 }
 

www/style-ultramodern.windows.css
@@ -52,6 +52,7 @@ body > * {
 	left: 0;
 	width: 100%;
 	height: 100%;
+	z-index: -1;
 	pointer-events: none;
 }
 

www/style-zero.linux.css
@@ -52,6 +52,7 @@ body > * {
 	left: 0;
 	width: 100%;
 	height: 100%;
+	z-index: -1;
 	pointer-events: none;
 }
 

www/style-zero.mac.css
@@ -52,6 +52,7 @@ body > * {
 	left: 0;
 	width: 100%;
 	height: 100%;
+	z-index: -1;
 	pointer-events: none;
 }
 

www/style-zero.windows.css
@@ -52,6 +52,7 @@ body > * {
 	left: 0;
 	width: 100%;
 	height: 100%;
+	z-index: -1;
 	pointer-events: none;
 }
 

www/style.css.php
@@ -74,6 +74,7 @@ body > * {
 	left: 0;
 	width: 100%;
 	height: 100%;
+	z-index: -1;
 	pointer-events: none;
 }
 

www/style.linux.css
@@ -52,6 +52,7 @@ body > * {
 	left: 0;
 	width: 100%;
 	height: 100%;
+	z-index: -1;
 	pointer-events: none;
 }
 

www/style.mac.css
@@ -52,6 +52,7 @@ body > * {
 	left: 0;
 	width: 100%;
 	height: 100%;
+	z-index: -1;
 	pointer-events: none;
 }
 

www/style.windows.css
@@ -52,6 +52,7 @@ body > * {
 	left: 0;
 	width: 100%;
 	height: 100%;
+	z-index: -1;
 	pointer-events: none;
 }
 