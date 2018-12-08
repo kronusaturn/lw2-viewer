079ffc903fe0ece61861b749f28f223a9cb2b77d
saturn <saturn@glxl.net>
2018-12-07 20:21:39 -0600

    Fix typo.

--- src/config-package.lisp
+++ src/config-package.lisp
@@ -1,4 +1,4 @@
-(uiop:define-package lw2-viewer.config
+(uiop:define-package #:lw2-viewer.config
   (:use #:cl #:lw2.sites #:lw2.backend-modules)
   (:export #:*lmdb-mapsize*)
   (:unintern #:*site-uri* #:*graphql-uri* #:*websocket-uri* #:*backend-type* #:*secure-cookies* #:*cache-db*))
