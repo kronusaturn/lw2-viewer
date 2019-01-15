0f00fc4cdae5d5ae60f4c60a43447618cfad0030
Said Achmiz <achmizs@gmail.com>
2019-01-15 02:43:52 -0500

    Accordius: styled in-post tag blocks for all themes

---
+++ www/accordius/theme-classic.php
@@ -0,0 +1,40 @@
+/*===============*/
+/* CLASSIC THEME */
+/*===============*/
+
+/*++++++*/
+/* TAGS */
+/*++++++*/
+
+#tags {
+    padding: 0.25em 0.5em;
+    line-height: 1;
+    justify-content: flex-start;
+    margin: 0.5em 0 0 0;
+}
+#tags::before {
+    content: "Tags:";
+    display: inline-block;
+    margin: 0 0.25em 0 0;
+    padding: 3px 0;
+    color: #999;
+}
+#tags a {
+    border-radius: 6px;
+    background-color: #ffd;
+    border: 1px solid #ecc700;
+    padding: 3px 6px;
+    line-height: 1;
+    margin: 0 0.25em;
+    font-size: 0.9375em;
+    text-decoration: none;
+    color: #6a8a6b;
+}
+#tags a:hover {
+    text-decoration: none;
+    background-color: #ecc700;
+    color: #000;
+}
+#tags a:active {
+    color: #d00;
+}
---
+++ www/accordius/theme-rts.php
@@ -0,0 +1,31 @@
+/*========================*/
+/* READTHESEQUENCES THEME */
+/*========================*/
+
+/*++++++*/
+/* TAGS */
+/*++++++*/
+
+#tags {
+    padding: 0.5em;
+    align-items: center;
+}
+#tags::before {
+    content: "\F02C";
+    font-family: Font Awesome;
+    font-weight: 400;
+    font-size: 0.875em;
+    display: inline-block;
+    margin: 0 0.5em 0 0;
+    opacity: 0.6;
+}
+#tags a {
+	margin: 0 0.375em 0 0;
+}
+#tags a:not(:last-of-type)::after {
+	content: ","
+}
+
+.post-page .post-meta::after {
+	order: 20;
+}
