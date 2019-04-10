kronusaturn/lw2-viewer

commit 37eafe6ddb9b6456070d062c237370bf029727a8

Fixed theme tweaker minimize bug

www/script.js
@@ -1608,7 +1608,7 @@ function injectThemeTweaker() {
 			themeTweakerStyleSheet.setAttribute('rel', 'stylesheet');
 			themeTweakerStyleSheet.setAttribute('href', '/css/theme_tweaker.css');
 			themeTweakerStyleSheet.addEventListener('load', GW.themeTweakerStyleSheetAvailable);
-			query("head").appendChild(themeTweakerStyleSheet);
+			query("head").insertBefore(themeTweakerStyleSheet, query("head").firstElementChild);
 		}
 	});
 }

commit 6f17e6265b549788a9214296a0a389d167e74cfa

Fixed regression caused by 37eafe6d

www/script.js
@@ -1069,7 +1069,7 @@ function setTheme(newThemeName) {
 	if (themeUnloadCallback != null) themeUnloadCallback(newThemeName);
 
 	let styleSheetNameSuffix = (newThemeName == 'default') ? '' : ('-' + newThemeName);
-	let currentStyleSheetNameComponents = /style[^\.]*(\..+)$/.exec(query("head link[href*='.css']").href);
+	let currentStyleSheetNameComponents = /style[^\.]*(\..+)$/.exec(query("head link[href*='/css/style'][href*='.css']").href);
 
 	let newStyle = document.createElement('link');
 	newStyle.setAttribute('rel', 'stylesheet');