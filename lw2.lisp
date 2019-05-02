d5749baefc5636a253189b1d9098fee4812bac52
saturn <saturn@glxl.net>
2019-05-02 18:59:42 -0500

    Add 30 second maximum deadline.

--- lw2.lisp
+++ lw2.lisp
@@ -673,8 +673,10 @@ signaled condition to OUT-STREAM."
 	       (if (or (eq (hunchentoot:request-method*) :post)
 		       (not (and (boundp '*test-acceptor*) (boundp '*hunchentoot-taskmaster*)))) ; TODO fix this hack
 		   (funcall fn)
-		   (sb-sys:with-deadline (:seconds (expt 1.3 (- (hunchentoot:taskmaster-max-thread-count (symbol-value '*hunchentoot-taskmaster*))
-								(hunchentoot::accessor-requests-in-progress (symbol-value '*test-acceptor*)))))
+		   (sb-sys:with-deadline (:seconds (expt 1.3
+							 (min (round (log 30 1.3))
+							      (- (hunchentoot:taskmaster-max-thread-count (symbol-value '*hunchentoot-taskmaster*))
+								 (hunchentoot::accessor-requests-in-progress (symbol-value '*test-acceptor*))))))
 		     (funcall fn))))
 	    (serious-condition (condition)
               (emit-page (out-stream :title "Error" :return-code (condition-http-return-code condition) :content-class "error-page")
