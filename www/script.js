bed6f07d71ac092f889454b286781c5150f0560a
achmizs <said@saidachmiz.net>
2019-05-18 16:33:09 -0400

    Fixed more GUIEdit bugs

--- www/script.js
+++ www/script.js
@@ -4658,17 +4658,17 @@ GW.guiEditButtons = [
 	[ 'strikethrough', 'Strike-through', '', '~~', '~~', 'Struck-out text', '&#xf0cc;' ],
 	[ 'link', 'Hyperlink', 'l', GUIEdit_hyperlink, '', '', '&#xf0c1;' ],
 	[ 'image', 'Image', '', GUIEdit_image, '', 'Image alt-text', '&#xf03e;' ],
-	[ 'heading1', 'Heading level 1', '', '\\n# ', '', 'Heading', '&#xf1dc;<sup>1</sup>' ],
-	[ 'heading2', 'Heading level 2', '', '\\n## ', '', 'Heading', '&#xf1dc;<sup>2</sup>' ],
-	[ 'heading3', 'Heading level 3', '', '\\n### ', '', 'Heading', '&#xf1dc;<sup>3</sup>' ],
+	[ 'heading1', 'Heading level 1', '', '\n# ', '', 'Heading', '&#xf1dc;<sup>1</sup>' ],
+	[ 'heading2', 'Heading level 2', '', '\n## ', '', 'Heading', '&#xf1dc;<sup>2</sup>' ],
+	[ 'heading3', 'Heading level 3', '', '\n### ', '', 'Heading', '&#xf1dc;<sup>3</sup>' ],
 	[ 'blockquote', 'Blockquote', 'q', GUIEdit_blockquote, '', '', '&#xf10e;' ],
-	[ 'bulleted-list', 'Bulleted list', '', '\\n* ', '', 'List item', '&#xf0ca;' ],
-	[ 'numbered-list', 'Numbered list', '', '\\n1. ', '', 'List item', '&#xf0cb;' ],
-	[ 'horizontal-rule', 'Horizontal rule', '', '\\n\\n---\\n\\n', '', '', '&#xf068;' ],
+	[ 'bulleted-list', 'Bulleted list', '', '\n* ', '', 'List item', '&#xf0ca;' ],
+	[ 'numbered-list', 'Numbered list', '', '\n1. ', '', 'List item', '&#xf0cb;' ],
+	[ 'horizontal-rule', 'Horizontal rule', '', '\n\n---\n\n', '', '', '&#xf068;' ],
 	[ 'inline-code', 'Inline code', '', '`', '`', 'Code', '&#xf121;' ],
-	[ 'code-block', 'Code block', '', '```\\n', '\\n```', 'Code', '&#xf1c9;' ],
+	[ 'code-block', 'Code block', '', '```\n', '\n```', 'Code', '&#xf1c9;' ],
 	[ 'formula', 'LaTeX', '', '$', '$', 'LaTeX formula', '&#xf155;' ],
-	[ 'spoiler', 'Spoiler block', '', '::: spoiler\\n', '\\n:::', 'Spoiler text', '&#xf2fc;' ],
+	[ 'spoiler', 'Spoiler block', '', '::: spoiler\n', '\n:::', 'Spoiler text', '&#xf2fc;' ],
 	[ 'footnote', 'Footnote', 'n', GUIEdit_footnote, '', '', '&#xf7e9;' ]
 ];
 
@@ -4712,7 +4712,7 @@ function GUIEdit_image(text, startpos) {
 		src = text;
 	} else {
 		alt_text = text;
-		src = prompt("Link address (URL):");
+		src = prompt("Image URL:");
 		if (!src) {
 			endpos = startpos + text.length;
 			return [ text, startpos, endpos ];
