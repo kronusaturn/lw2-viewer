2c2695b807c59cb0a4526c0d0fccedb7d8ff1850
achmizs <said@saidachmiz.net>
2019-05-19 15:05:47 -0400

    Fixed typo

--- www/js-components/new_comment_nav_ui.js
+++ www/js-components/new_comment_nav_ui.js
@@ -1,5 +1,5 @@
 `<div id='new-comment-nav-ui'>
 	<button type='button' class='new-comment-sequential-nav-button new-comment-previous' title='Previous new comment (,)' tabindex='-1'>&#xf0d8;</button>
 	<span class='new-comments-count'></span>
-	<button type='button' class='new-comment-sequential-nav-button new-comment-next' title='Next new comment (.)' tabindex='-1'>&#xf0d7;</button>`
+	<button type='button' class='new-comment-sequential-nav-button new-comment-next' title='Next new comment (.)' tabindex='-1'>&#xf0d7;</button>
 </div>`
--- www/js/script.js
+++ www/js/script.js
@@ -1461,7 +1461,7 @@ function injectNewCommentNavUI(newCommentsCount) {
 	let newCommentNavUIContainer = addUIElement(`<div id='new-comment-nav-ui'>
 	<button type='button' class='new-comment-sequential-nav-button new-comment-previous' title='Previous new comment (,)' tabindex='-1'>&#xf0d8;</button>
 	<span class='new-comments-count'></span>
-	<button type='button' class='new-comment-sequential-nav-button new-comment-next' title='Next new comment (.)' tabindex='-1'>&#xf0d7;</button>`
+	<button type='button' class='new-comment-sequential-nav-button new-comment-next' title='Next new comment (.)' tabindex='-1'>&#xf0d7;</button>
 </div>`);
 
 	newCommentNavUIContainer.queryAll(".new-comment-sequential-nav-button").forEach(button => {
