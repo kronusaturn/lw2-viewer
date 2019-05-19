e15dcc4accbdb99dd4766dcfd2afdbc1ca3b5c7e
achmizs <said@saidachmiz.net>
2019-05-19 18:37:24 -0400

    JS templating system, part III

--- assemble_js.php
+++ assemble_js.php
@@ -3,11 +3,14 @@
 $js_components_dir = "www/js-components/";
 $js_dir = "www/js/";
 
-$js_file = preg_replace_callback("/{{{(.+?)}}}/", function ($m) {
-	global $js_components_dir;
-	return file_get_contents($js_components_dir."{$m[1]}.js");
-}, file_get_contents($argv[1]));
+$js_files = glob("www/*.js");
+foreach ($js_files as $js_file_name) {
+	$js_file = preg_replace_callback("/{{{(.+?)}}}/", function ($m) {
+		global $js_components_dir;
+		return file_get_contents($js_components_dir."{$m[1]}.js");
+	}, file_get_contents($js_file_name));
 
-file_put_contents("{$js_dir}". basename($argv[1]), $js_file);
+	file_put_contents("{$js_dir}". basename($js_file_name), $js_file);
+}
 
 ?>
