5bdce75677b0f64e24c0b04048b5e650b22c3d24
achmizs <said@saidachmiz.net>
2019-05-21 04:17:27 -0400

    Made assemble_js.php recursive, to allow nested components

--- assemble_js.php
+++ assemble_js.php
@@ -4,14 +4,15 @@
 $js_components_dir = "www/js-components/";
 $js_dir = "www/js/";
 
-$js_files = glob("www/*.js");
-foreach ($js_files as $js_file_name) {
-	$js_file = preg_replace_callback("/{{{(.+?)}}}/", function ($m) {
-		global $js_components_dir;
-		return file_get_contents($js_components_dir."{$m[1]}.js");
-	}, file_get_contents($js_file_name));
+foreach (glob("www/*.js") as $js_file_name) {
+	file_put_contents($js_dir.basename($js_file_name), process_file(file_get_contents($js_file_name)));
+}
 
-	file_put_contents("{$js_dir}". basename($js_file_name), $js_file);
+function process_file($js_file) {
+	return preg_replace_callback("/{{{(.+?)}}}/", function ($m) {
+		global $js_components_dir;
+		return process_file(file_get_contents($js_components_dir."{$m[1]}.js"));
+	}, $js_file);
 }
 
 ?>
