321529c2b6f058bfeeec43d65b24975ce6cc2237
saturn <saturn@glxl.net>
2019-06-08 19:39:26 -0500

    Fix username display and *arbital-context* scope.

--- arbital.lisp
+++ arbital.lisp
@@ -58,20 +58,21 @@
     (write-sequence (clean-html* (markdown:parse markdown)) stream)))
 
 (defun arbital-meta-block (page-data all-data type)
- (let ((creator-id (cdr (assoc :page-creator-id page-data))))
-   <a class="author" href=("/p/~A" creator-id) data-userid=creator-id>
-     (cdr (assoc :title (cdr (assoc creator-id (cdr (assoc :pages all-data)) :test #'string=))))
-   </a>)
- (multiple-value-bind (pretty-time js-time) (pretty-time (cdr (assoc :page-created-at page-data)) :loose-parsing t)
-   (cond
-     ((eq type :comment)
-      <a class="date" href=("#comment-~A" (cdr (assoc :page-id page-data))) data-js-date=js-time>
-        (progn pretty-time)
-      </a>)
-     (t
-      <span class="date" data-js-date=js-time>
-        (progn pretty-time)
-      </span>))))
+  (let* ((creator-id (cdr (assoc :page-creator-id page-data)))
+	 (user (cdr (assoc creator-id (cdr (assoc :users all-data)) :test #'string=))))
+    <a class="author" href=("/p/~A" creator-id) data-userid=creator-id>
+      (format nil "~{~A~^ ~}" (map 'list (lambda (x) (cdr (assoc x user))) '(:first-name :last-name)))
+    </a>)
+  (multiple-value-bind (pretty-time js-time) (pretty-time (cdr (assoc :page-created-at page-data)) :loose-parsing t)
+    (cond
+      ((eq type :comment)
+       <a class="date" href=("#comment-~A" (cdr (assoc :page-id page-data))) data-js-date=js-time>
+         (progn pretty-time)
+       </a>)
+      (t
+       <span class="date" data-js-date=js-time>
+         (progn pretty-time)
+       </span>))))
 
 (define-component view-arbital-page (id page-alias page-type)
   (:http-args '((l :type (or string null))))
@@ -87,59 +88,59 @@
 			  (cdr (assoc :pages all-data))
 			  :test #'string=))))
     (renderer ()
-      (emit-page (*html-output*)
-	<main class="post">
-	  <h1 class="post-title">(cdr (assoc :title page-data))</h1>
-	  <div class="post-meta">
-	    (arbital-meta-block page-data all-data :page)
-	  </div>
-          <div class="body-text post-body">
-	    (with-html-stream-output
-	        (when (assoc :text page-data)
-	          (let ((*arbital-context* (cdr (assoc :pages all-data))))
+      (let ((*arbital-context* (cdr (assoc :pages all-data))))
+	(emit-page (*html-output*)
+	    <main class="post">
+	    <h1 class="post-title">(cdr (assoc :title page-data))</h1>
+	    <div class="post-meta">
+		(arbital-meta-block page-data all-data :page)
+	    </div>
+	    <div class="body-text post-body">
+		(with-html-stream-output
+		    (when (assoc :text page-data)
 		    (arbital-markdown-to-html (cdr (assoc :text page-data))
-					      *html-output*))))
-	    (dolist (page-list-data '((:child-ids "Children")
-				      (:parent-ids "Parents")))
-	      (destructuring-bind (page-list-id page-list-name) page-list-data
-	        <p>(progn page-list-name):
-		  (labels
-		      ((list-pages (page-list)
-	                 <ul>
-	                   (dolist (c page-list)
-	                     (let ((page-data (cdr (assoc c (cdr (assoc :pages all-data)) :test #'string=))))
-	                       <li><a href=("/p/~A~@[?l=~A~]" (cdr (assoc :alias page-data)) c)>(cdr (assoc :title page-data))</a>
-			         (when-let (page-list (cdr (assoc page-list-id page-data)))
-					   (list-pages page-list))
-			       </li>))
-			   </ul>))
-		    (when-let (page-list (cdr (assoc page-list-id page-data)))
-			      (list-pages page-list)))
-	        </p>))
-	  </div>
-	</main>
-	<div class="comments" id="comments">
-	  (labels ((arbital-comments (comment-list depth)
-		     <ul class="comment-thread">
-		       (dolist (c comment-list)
-		         (let ((comment-data (cdr (assoc c (cdr (assoc :pages all-data)) :test #'string=))))
-			   <li class=("comment-item ~A" (if (evenp depth) "depth-odd" "depth-even")) id=("#comment-~A" (cdr (assoc :page-id comment-data)))>
-			     <div class="comment">
-			       <div class="comment-meta">
-			         (arbital-meta-block comment-data all-data :comment)
-			       </div>
-			       <div class="comment-body body-text">
-			         (with-html-stream-output
-				     (arbital-markdown-to-html (cdr (assoc :text comment-data)) *html-output*))
-			       </div>
-			     </div>
-			     (when-let (comment-list (cdr (assoc :comment-ids comment-data)))
-				       (arbital-comments comment-list (1+ depth)))
-			   </li>))
-		     </ul>))
-	    (when-let (comment-list (cdr (assoc :comment-ids page-data)))
-		      (arbital-comments comment-list 0)))
-	</div>))))
+						*html-output*)))
+		(dolist (page-list-data '((:child-ids "Children")
+					(:parent-ids "Parents")))
+		(destructuring-bind (page-list-id page-list-name) page-list-data
+		    <p>(progn page-list-name):
+		    (labels
+			((list-pages (page-list)
+			    <ul>
+			    (dolist (c page-list)
+				(let ((page-data (cdr (assoc c (cdr (assoc :pages all-data)) :test #'string=))))
+				<li><a href=("/p/~A~@[?l=~A~]" (cdr (assoc :alias page-data)) c)>(cdr (assoc :title page-data))</a>
+				    (when-let (page-list (cdr (assoc page-list-id page-data)))
+					    (list-pages page-list))
+				</li>))
+			    </ul>))
+			(when-let (page-list (cdr (assoc page-list-id page-data)))
+				(list-pages page-list)))
+		    </p>))
+	    </div>
+	    </main>
+	    <div class="comments" id="comments">
+	    (labels ((arbital-comments (comment-list depth)
+			<ul class="comment-thread">
+			(dolist (c comment-list)
+			    (let ((comment-data (cdr (assoc c (cdr (assoc :pages all-data)) :test #'string=))))
+			    <li class=("comment-item ~A" (if (evenp depth) "depth-odd" "depth-even")) id=("#comment-~A" (cdr (assoc :page-id comment-data)))>
+				<div class="comment">
+				<div class="comment-meta">
+				    (arbital-meta-block comment-data all-data :comment)
+				</div>
+				<div class="comment-body body-text">
+				    (with-html-stream-output
+					(arbital-markdown-to-html (cdr (assoc :text comment-data)) *html-output*))
+				</div>
+				</div>
+				(when-let (comment-list (cdr (assoc :comment-ids comment-data)))
+					(arbital-comments comment-list (1+ depth)))
+			    </li>))
+			</ul>))
+		(when-let (comment-list (cdr (assoc :comment-ids page-data)))
+			(arbital-comments comment-list 0)))
+	    </div>)))))
 
 (define-route 'arbital-site 'standard-route :name 'view-arbital-root :uri "/" :handler (route-component view-arbital-page () nil "84c" :primary-page))
 (define-route 'arbital-site 'regex-route :name 'view-arbital-page :regex "/p/([^/]+)" :handler (route-component view-arbital-page (page-alias) nil page-alias :primary-page))
