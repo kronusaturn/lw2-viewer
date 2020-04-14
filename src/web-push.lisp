acd0aafc63f49d7335456e642c663ceb1f994b56
saturn <saturn@glxl.net>
2020-04-14 18:44:49 -0500

    Fix error in 0c858e44

--- src/web-push.lisp
+++ src/web-push.lisp
@@ -72,9 +72,8 @@
 
 (defun send-notification (endpoint &key (ttl (* 60 60 24)))
   ;; we don't support content yet since it requires encryption
-  (with-connection-pool
-      (dex:request endpoint
-		   :method :post
-		   :headers (list* (cons :ttl ttl)
-				   (cons :content-encoding "identity")
-				   (get-vapid-headers (quri:render-uri (quri:merge-uris "/" endpoint)))))))
+  (dex:request endpoint
+	       :method :post
+	       :headers (list* (cons :ttl ttl)
+			       (cons :content-encoding "identity")
+			       (get-vapid-headers (quri:render-uri (quri:merge-uris "/" endpoint))))))
