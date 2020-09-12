kronusaturn/lw2-viewer

commit 4ea23779cf38f4f84408c119fa4c8fbd27498e76

Clean up identify-item.

lw2.lisp
@@ -418,19 +418,17 @@ signaled condition to OUT-STREAM."
             collect x))))
 
 (defun identify-item (x)
-  (cond
-    ((typep x 'condition)
-     :condition)
-    ((assoc :message x)
-     :notification)
-    ((string= (cdr (assoc :----typename x)) "Message")
-     :message)
-    ((string= (cdr (assoc :----typename x)) "Conversation")
-     :conversation)
-    ((assoc :comment-count x)
-     :post)
-    (t
-     :comment)))
+  (typecase x
+    (list
+     (alexandria:if-let (typename (cdr (assoc :----typename x)))
+			(find-symbol (string-upcase typename) (find-package :keyword))
+			(cond
+			  ((assoc :message x)
+			   :notification)
+			  ((assoc :comment-count x)
+			   :post)
+			  (t
+			   :comment))))))
 
 (defun write-index-items-to-html (out-stream items &key need-auth (empty-message "No entries.") skip-section)
   (if items

commit d53ea351eecf533e888a6b08f0a63887c4b9e4f5

Fix bug introduced in 4ea23779c.

lw2.lisp
@@ -351,7 +351,8 @@ signaled condition to *HTML-OUTPUT*."
 	       ((assoc :comment-count x)
 		:post)
 	       (t
-		:comment))))))
+		:comment))))
+    (condition :condition)))
 
 (defun write-index-items-to-html (out-stream items &key need-auth (empty-message "No entries.") skip-section)
   (if items