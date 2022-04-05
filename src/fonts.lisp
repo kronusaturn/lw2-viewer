e57538f5db91b926fe0169cb9cc9f3d8f58b07b6
saturn <saturn@glxl.net>
2022-04-04 19:53:07 -0500

    Remove proxy for obormot font server.

--- src/fonts.lisp
+++ src/fonts.lisp
@@ -20,8 +20,8 @@
 ;;;; obormot-fonts-source
 
 (defparameter *obormot-fonts-stylesheet-uris*
-  '("https://fonts.greaterwrong.com/?fonts=InconsolataGW,CharterGW,ConcourseGW,Whitney,MundoSans,SourceSansPro,Raleway,ProximaNova,TiredOfCourier,AnonymousPro,InputSans,InputSansNarrow,InputSansCondensed,GaramondPremierPro,TriplicateCode,TradeGothic,NewsGothicBT,Caecilia,SourceSerifPro,SourceCodePro"
-    "https://fonts.greaterwrong.com/?fonts=BitmapFonts,FontAwesomeGW&base64encode=1"))
+  '("https://fonts.obormot.net/?fonts=InconsolataGW,CharterGW,ConcourseGW,Whitney,MundoSans,SourceSansPro,Raleway,ProximaNova,TiredOfCourier,AnonymousPro,InputSans,InputSansNarrow,InputSansCondensed,GaramondPremierPro,TriplicateCode,TradeGothic,NewsGothicBT,Caecilia,SourceSerifPro,SourceCodePro"
+    "https://fonts.obormot.net/?fonts=BitmapFonts,FontAwesomeGW&base64encode=1"))
 ;(defparameter *obormot-fonts-stylesheet-uris* '("https://fonts.greaterwrong.com/?fonts=*"))
 
 (defvar *fonts-redirect-data* nil)
