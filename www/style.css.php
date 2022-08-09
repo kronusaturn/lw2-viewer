kronusaturn/lw2-viewer

commit 6780e8b37224ca93edf365c850d938327aa297aa

Make more room for voting controls on narrow screens.

www/css/style-brutalist.linux.css
@@ -5505,10 +5505,22 @@ a.comment-parent-link::after {
 }
 @media only screen and (max-width: 520px) {
 	.comment-meta {
-		padding: 2px 10px;
+		padding: 2px 24px 2px 10px;
 		position: relative;
 	}
-	.comment-meta .author {
+	.comment-meta > * {
+		order: 3;
+	}
+	.comment-meta > :not(.author) {
+		line-height: 1.8;
+	}
+	.comment-meta .author,
+	.comment-meta .date {
+		order: 1;
+	}
+	.comment-meta:before {
+		content: "";
+		order: 2;
 		flex-basis: 100%;
 	}
 	.comment-post-title2 {

www/css/style-brutalist.mac.css
@@ -5505,10 +5505,22 @@ a.comment-parent-link::after {
 }
 @media only screen and (max-width: 520px) {
 	.comment-meta {
-		padding: 2px 10px;
+		padding: 2px 24px 2px 10px;
 		position: relative;
 	}
-	.comment-meta .author {
+	.comment-meta > * {
+		order: 3;
+	}
+	.comment-meta > :not(.author) {
+		line-height: 1.8;
+	}
+	.comment-meta .author,
+	.comment-meta .date {
+		order: 1;
+	}
+	.comment-meta:before {
+		content: "";
+		order: 2;
 		flex-basis: 100%;
 	}
 	.comment-post-title2 {

www/css/style-brutalist.windows.css
@@ -5505,10 +5505,22 @@ a.comment-parent-link::after {
 }
 @media only screen and (max-width: 520px) {
 	.comment-meta {
-		padding: 2px 10px;
+		padding: 2px 24px 2px 10px;
 		position: relative;
 	}
-	.comment-meta .author {
+	.comment-meta > * {
+		order: 3;
+	}
+	.comment-meta > :not(.author) {
+		line-height: 1.8;
+	}
+	.comment-meta .author,
+	.comment-meta .date {
+		order: 1;
+	}
+	.comment-meta:before {
+		content: "";
+		order: 2;
 		flex-basis: 100%;
 	}
 	.comment-post-title2 {

www/css/style-classic.linux.css
@@ -5505,10 +5505,22 @@ a.comment-parent-link::after {
 }
 @media only screen and (max-width: 520px) {
 	.comment-meta {
-		padding: 2px 10px;
+		padding: 2px 24px 2px 10px;
 		position: relative;
 	}
-	.comment-meta .author {
+	.comment-meta > * {
+		order: 3;
+	}
+	.comment-meta > :not(.author) {
+		line-height: 1.8;
+	}
+	.comment-meta .author,
+	.comment-meta .date {
+		order: 1;
+	}
+	.comment-meta:before {
+		content: "";
+		order: 2;
 		flex-basis: 100%;
 	}
 	.comment-post-title2 {

www/css/style-classic.mac.css
@@ -5505,10 +5505,22 @@ a.comment-parent-link::after {
 }
 @media only screen and (max-width: 520px) {
 	.comment-meta {
-		padding: 2px 10px;
+		padding: 2px 24px 2px 10px;
 		position: relative;
 	}
-	.comment-meta .author {
+	.comment-meta > * {
+		order: 3;
+	}
+	.comment-meta > :not(.author) {
+		line-height: 1.8;
+	}
+	.comment-meta .author,
+	.comment-meta .date {
+		order: 1;
+	}
+	.comment-meta:before {
+		content: "";
+		order: 2;
 		flex-basis: 100%;
 	}
 	.comment-post-title2 {

www/css/style-classic.windows.css
@@ -5505,10 +5505,22 @@ a.comment-parent-link::after {
 }
 @media only screen and (max-width: 520px) {
 	.comment-meta {
-		padding: 2px 10px;
+		padding: 2px 24px 2px 10px;
 		position: relative;
 	}
-	.comment-meta .author {
+	.comment-meta > * {
+		order: 3;
+	}
+	.comment-meta > :not(.author) {
+		line-height: 1.8;
+	}
+	.comment-meta .author,
+	.comment-meta .date {
+		order: 1;
+	}
+	.comment-meta:before {
+		content: "";
+		order: 2;
 		flex-basis: 100%;
 	}
 	.comment-post-title2 {

www/css/style-dark.linux.css
@@ -5508,10 +5508,22 @@ a.comment-parent-link::after {
 }
 @media only screen and (max-width: 520px) {
 	.comment-meta {
-		padding: 2px 10px;
+		padding: 2px 24px 2px 10px;
 		position: relative;
 	}
-	.comment-meta .author {
+	.comment-meta > * {
+		order: 3;
+	}
+	.comment-meta > :not(.author) {
+		line-height: 1.8;
+	}
+	.comment-meta .author,
+	.comment-meta .date {
+		order: 1;
+	}
+	.comment-meta:before {
+		content: "";
+		order: 2;
 		flex-basis: 100%;
 	}
 	.comment-post-title2 {

www/css/style-dark.mac.css
@@ -5508,10 +5508,22 @@ a.comment-parent-link::after {
 }
 @media only screen and (max-width: 520px) {
 	.comment-meta {
-		padding: 2px 10px;
+		padding: 2px 24px 2px 10px;
 		position: relative;
 	}
-	.comment-meta .author {
+	.comment-meta > * {
+		order: 3;
+	}
+	.comment-meta > :not(.author) {
+		line-height: 1.8;
+	}
+	.comment-meta .author,
+	.comment-meta .date {
+		order: 1;
+	}
+	.comment-meta:before {
+		content: "";
+		order: 2;
 		flex-basis: 100%;
 	}
 	.comment-post-title2 {

commit edaecc140d2772d58e5dda6c6708d5a27dc408aa

Fix layout bug introduced in 6780e8b3.

www/css/style-brutalist.linux.css
@@ -2311,6 +2311,7 @@ ul.comment-thread {
 	flex-basis: 100%;
 	overflow: hidden;
 	text-overflow: ellipsis;
+	white-space: nowrap;
 	line-height: 1.3;
 }
 .conversation-page .comment-meta .comment-post-title {

www/css/style-brutalist.mac.css
@@ -2311,6 +2311,7 @@ ul.comment-thread {
 	flex-basis: 100%;
 	overflow: hidden;
 	text-overflow: ellipsis;
+	white-space: nowrap;
 	line-height: 1.3;
 }
 .conversation-page .comment-meta .comment-post-title {

www/css/style-brutalist.windows.css
@@ -2311,6 +2311,7 @@ ul.comment-thread {
 	flex-basis: 100%;
 	overflow: hidden;
 	text-overflow: ellipsis;
+	white-space: nowrap;
 	line-height: 1.3;
 }
 .conversation-page .comment-meta .comment-post-title {

www/css/style-classic.linux.css
@@ -2311,6 +2311,7 @@ ul.comment-thread {
 	flex-basis: 100%;
 	overflow: hidden;
 	text-overflow: ellipsis;
+	white-space: nowrap;
 	line-height: 1.3;
 }
 .conversation-page .comment-meta .comment-post-title {

www/css/style-classic.mac.css
@@ -2311,6 +2311,7 @@ ul.comment-thread {
 	flex-basis: 100%;
 	overflow: hidden;
 	text-overflow: ellipsis;
+	white-space: nowrap;
 	line-height: 1.3;
 }
 .conversation-page .comment-meta .comment-post-title {

www/css/style-classic.windows.css
@@ -2311,6 +2311,7 @@ ul.comment-thread {
 	flex-basis: 100%;
 	overflow: hidden;
 	text-overflow: ellipsis;
+	white-space: nowrap;
 	line-height: 1.3;
 }
 .conversation-page .comment-meta .comment-post-title {

www/css/style-dark.linux.css
@@ -2314,6 +2314,7 @@ ul.comment-thread {
 	flex-basis: 100%;
 	overflow: hidden;
 	text-overflow: ellipsis;
+	white-space: nowrap;
 	line-height: 1.3;
 }
 .conversation-page .comment-meta .comment-post-title {

www/css/style-dark.mac.css
@@ -2314,6 +2314,7 @@ ul.comment-thread {
 	flex-basis: 100%;
 	overflow: hidden;
 	text-overflow: ellipsis;
+	white-space: nowrap;
 	line-height: 1.3;
 }
 .conversation-page .comment-meta .comment-post-title {

www/css/style-dark.windows.css
@@ -2314,6 +2314,7 @@ ul.comment-thread {
 	flex-basis: 100%;
 	overflow: hidden;
 	text-overflow: ellipsis;
+	white-space: nowrap;
 	line-height: 1.3;
 }
 .conversation-page .comment-meta .comment-post-title {

www/css/style-grey.linux.css
@@ -2311,6 +2311,7 @@ ul.comment-thread {
 	flex-basis: 100%;
 	overflow: hidden;
 	text-overflow: ellipsis;
+	white-space: nowrap;
 	line-height: 1.3;
 }
 .conversation-page .comment-meta .comment-post-title {

www/css/style-grey.mac.css
@@ -2311,6 +2311,7 @@ ul.comment-thread {
 	flex-basis: 100%;
 	overflow: hidden;
 	text-overflow: ellipsis;
+	white-space: nowrap;
 	line-height: 1.3;
 }
 .conversation-page .comment-meta .comment-post-title {

www/css/style-grey.windows.css
@@ -2311,6 +2311,7 @@ ul.comment-thread {
 	flex-basis: 100%;
 	overflow: hidden;
 	text-overflow: ellipsis;
+	white-space: nowrap;
 	line-height: 1.3;
 }
 .conversation-page .comment-meta .comment-post-title {

www/css/style-less.linux.css
@@ -2311,6 +2311,7 @@ ul.comment-thread {
 	flex-basis: 100%;
 	overflow: hidden;
 	text-overflow: ellipsis;
+	white-space: nowrap;
 	line-height: 1.3;
 }
 .conversation-page .comment-meta .comment-post-title {

www/css/style-less.mac.css
@@ -2311,6 +2311,7 @@ ul.comment-thread {
 	flex-basis: 100%;
 	overflow: hidden;
 	text-overflow: ellipsis;
+	white-space: nowrap;
 	line-height: 1.3;
 }
 .conversation-page .comment-meta .comment-post-title {

www/css/style-less.windows.css
@@ -2311,6 +2311,7 @@ ul.comment-thread {
 	flex-basis: 100%;
 	overflow: hidden;
 	text-overflow: ellipsis;
+	white-space: nowrap;
 	line-height: 1.3;
 }
 .conversation-page .comment-meta .comment-post-title {

www/css/style-rts.linux.css
@@ -2311,6 +2311,7 @@ ul.comment-thread {
 	flex-basis: 100%;
 	overflow: hidden;
 	text-overflow: ellipsis;
+	white-space: nowrap;
 	line-height: 1.3;
 }
 .conversation-page .comment-meta .comment-post-title {

www/css/style-rts.mac.css
@@ -2311,6 +2311,7 @@ ul.comment-thread {
 	flex-basis: 100%;
 	overflow: hidden;
 	text-overflow: ellipsis;
+	white-space: nowrap;
 	line-height: 1.3;
 }
 .conversation-page .comment-meta .comment-post-title {

www/css/style-rts.windows.css
@@ -2311,6 +2311,7 @@ ul.comment-thread {
 	flex-basis: 100%;
 	overflow: hidden;
 	text-overflow: ellipsis;
+	white-space: nowrap;
 	line-height: 1.3;
 }
 .conversation-page .comment-meta .comment-post-title {

www/css/style-ultramodern.linux.css
@@ -2311,6 +2311,7 @@ ul.comment-thread {
 	flex-basis: 100%;
 	overflow: hidden;
 	text-overflow: ellipsis;
+	white-space: nowrap;
 	line-height: 1.3;
 }
 .conversation-page .comment-meta .comment-post-title {

www/css/style-ultramodern.mac.css
@@ -2311,6 +2311,7 @@ ul.comment-thread {
 	flex-basis: 100%;
 	overflow: hidden;
 	text-overflow: ellipsis;
+	white-space: nowrap;
 	line-height: 1.3;
 }
 .conversation-page .comment-meta .comment-post-title {

www/css/style-ultramodern.windows.css
@@ -2311,6 +2311,7 @@ ul.comment-thread {
 	flex-basis: 100%;
 	overflow: hidden;
 	text-overflow: ellipsis;
+	white-space: nowrap;
 	line-height: 1.3;
 }
 .conversation-page .comment-meta .comment-post-title {

www/css/style-zero.linux.css
@@ -2311,6 +2311,7 @@ ul.comment-thread {
 	flex-basis: 100%;
 	overflow: hidden;
 	text-overflow: ellipsis;
+	white-space: nowrap;
 	line-height: 1.3;
 }
 .conversation-page .comment-meta .comment-post-title {

www/css/style-zero.mac.css
@@ -2311,6 +2311,7 @@ ul.comment-thread {
 	flex-basis: 100%;
 	overflow: hidden;
 	text-overflow: ellipsis;
+	white-space: nowrap;
 	line-height: 1.3;
 }
 .conversation-page .comment-meta .comment-post-title {

www/css/style-zero.windows.css
@@ -2311,6 +2311,7 @@ ul.comment-thread {
 	flex-basis: 100%;
 	overflow: hidden;
 	text-overflow: ellipsis;
+	white-space: nowrap;
 	line-height: 1.3;
 }
 .conversation-page .comment-meta .comment-post-title {

www/css/style.linux.css
@@ -2311,6 +2311,7 @@ ul.comment-thread {
 	flex-basis: 100%;
 	overflow: hidden;
 	text-overflow: ellipsis;
+	white-space: nowrap;
 	line-height: 1.3;
 }
 .conversation-page .comment-meta .comment-post-title {

www/css/style.mac.css
@@ -2311,6 +2311,7 @@ ul.comment-thread {
 	flex-basis: 100%;
 	overflow: hidden;
 	text-overflow: ellipsis;
+	white-space: nowrap;
 	line-height: 1.3;
 }
 .conversation-page .comment-meta .comment-post-title {

www/css/style.windows.css
@@ -2311,6 +2311,7 @@ ul.comment-thread {
 	flex-basis: 100%;
 	overflow: hidden;
 	text-overflow: ellipsis;
+	white-space: nowrap;
 	line-height: 1.3;
 }
 .conversation-page .comment-meta .comment-post-title {

www/style.css.php
@@ -2322,6 +2322,7 @@ ul.comment-thread {
 	flex-basis: 100%;
 	overflow: hidden;
 	text-overflow: ellipsis;
+	white-space: nowrap;
 	line-height: 1.3;
 }
 .conversation-page .comment-meta .comment-post-title {